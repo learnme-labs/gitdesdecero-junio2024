6244ec99428a4c00f85a5083b4a7c8fda2020e93
GIT Teacher <oscar.marq.olv@gmail.com>
2024-06-06 10:49:16 -0600

    [ FEATURE ] API de registro LOG
    Nuevos endpoints
    * `GET:/logs`
    
     Changes to be committed:
    	modified:   app-nodejs-testing/app.js
    	new file:   app-nodejs-testing/routes/log.js

--- app-nodejs-testing/app.js
+++ app-nodejs-testing/app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var accountsRouter = require('./routes/account');
+var accountsRouter = require('./routes/accounts');
+var logsRouter = require('./routes/log');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/accounts', accountsRouter);
+app.use('/logs', logsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
